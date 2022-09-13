31476c7559f66b4b7ac9ee74363e27541e2edb5b
traceWater <dalecottrell62@gmail.com>
2022-09-13 15:43:10 -0700

    added signature js

--- src/components/SignatureDrinks/SignatureDrinksItem.js
+++ src/components/SignatureDrinks/SignatureDrinksItem.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
-// import classes from './SignatureDrinksItem.module.css'
+//styles
+import "./SignatureDrinks.css";
 
-const SignatureDrinksItem = () => {
+function SignatureDrinks() {
   return (
-    <div>Signature Drinks Item</div>
-  )
+    <div className="SignatureDrinksItem-container">
+      <h1 className="SignatureDrinksItem-title">.</h1>
+      <div className="SignatureDrinksItem-image-container">
+        <img
+          className="SignatureDrinksItem-image"
+          src={process.env.PUBLIC_URL + "/Public_Images/margarita.png"}
+          alt="tea pot"
+        />
+        <img
+          className="SignatureDrinksItem-image"
+          src={process.env.PUBLIC_URL + "/Public_Images/paloma.png"}
+          alt="one eye"
+        />
+        <img
+          className="SignatureDrinksItem-image"
+          src={process.env.PUBLIC_URL + "/Public_Images/mojito.png"}
+          alt="tea cup"
+        />
+      </div>
+      <a
+        className="SignatureDrinksItem-link"
+        href="https://www.instagram.com/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        .
+      </a>
+    </div>
+  );
 }
 
-export default SignatureDrinksItem
+export default SignatureDrinks;
