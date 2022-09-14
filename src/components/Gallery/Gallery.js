57da161e04bd0c1d401a107594d71e65762700b1
exionite <exionite@gmail.com>
2022-09-14 10:14:42 -0700

    add gallery layout for videos

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,7 +5,14 @@ import classes from './Gallery.module.css'
 const Gallery = () => {
   return (
     <section className={classes.gallery}>
-      
+      <div className={classes.pic1}></div>
+      <div className={classes.pic2}></div>
+      <div className={classes.pic3}></div>
+      <div className={classes.pic4}></div>
+      <div className={classes.pic5}></div>
+      <div className={classes.pic6}></div>
+      <div className={classes.pic7}></div>
+
       {/* <div className={classes.row}>
         <div className={classes.column}>
           <img src={`${process.env.PUBLIC_URL}/images/galleryImg/1.gif`}
