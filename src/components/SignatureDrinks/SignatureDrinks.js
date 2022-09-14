921acdb480d16ac9b8b3318b94891101395de665
traceWater <dalecottrell62@gmail.com>
2022-09-14 15:53:50 -0700

    added signatureDrinksCard

--- src/components/SignatureDrinks/SignatureDrinks.js
+++ src/components/SignatureDrinks/SignatureDrinks.js
@@ -1,22 +1,16 @@
-import React from "react";
-import { DRINKS_DATA } from '../../data2/drinks-data';
-import classes from './SignatureDrinks.module.css';
+import React from 'react';
 
-function SignatureDrinks() {
+
+import SignatureDrinksDisplay from './SignatureDrinksDisplay.js';
+
+
+const SignatureDrinks = () => {
   return (
-    <section className={classes.signatureDrinks__container} id="drinks">
-      <h1>Signature Drinks</h1>
-    <div className={classes.signatureDrinksItem__container}>
-      <div className={classes.signatureDrinksItem__container}>
-        <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/margarita.png`} alt="" />
-        <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/paloma.png`} alt="" />
-        <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/mojito.png`} alt="" />
-      </div>
-      <a className={classes.signatureDrinksItem__Link} href="https://www.instagram.com/" rel="noopener noreferrer" target="_blank" ></a>
-    </div>
+    <section>
+      <h2>Signature Drinks</h2>
+      <SignatureDrinksDisplay />
     </section>
   );
-}
+};
 
 export default SignatureDrinks;
-
---
+++ src/components/SignatureDrinks/SignatureDrinksEDIT.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import { DRINKS_DATA } from '../../data2/drinks-data';
+// import classes from './SignatureDrinks.module.css';
+
+// function SignatureDrinks() {
+//   return (
+//     <section className={classes.signatureDrinks__container} id="drinks">
+//       <h1>Signature Drinks</h1>
+//     <div className={classes.signatureDrinksItem__container}>
+//       <div className={classes.signatureDrinksItem__container}>
+//         <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/margarita.png`} alt="" />
+//         <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/paloma.png`} alt="" />
+//         <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/mojito.png`} alt="" />
+//       </div>
+//       <a className={classes.signatureDrinksItem__Link} href="https://www.instagram.com/" rel="noopener noreferrer" target="_blank" ></a>
+//     </div>
+//     </section>
+//   );
+// }
+
+// export default SignatureDrinks;
+
