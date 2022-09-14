48f624f9ed8c88c204bcbbc2e55907d63efab024
exionite <exionite@gmail.com>
2022-09-14 15:56:44 -0700

    merge with main

---
+++ src/store/modal-context.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+//* The Modal Context Store, where we can activate the modal
+
+const ModalContext = React.createContext({
+  showModal: false,
+})
+
+export const ModalContextProvider = (props) => {
+  
+}
