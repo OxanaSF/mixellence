b7930dcf035f8b0ad7b27193c37d8ff1f027dfe2
traceWater <dalecottrell62@gmail.com>
2022-09-14 12:35:19 -0700

    updated drinks-data

--- src/components/SignatureDrinks/SignatureDrinks.js
+++ src/components/SignatureDrinks/SignatureDrinks.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { DRINK_DATA2 } from '../../data2/drinks-data2';
+import { DRINKS_DATA } from '../../data2/drinks-data';
 import classes from './SignatureDrinks.module.css';
 
 function SignatureDrinks() {
   return (
     <section className={classes.signatureDrinks__container} id="drinks">
+      <h1>Signature Drinks</h1>
     <div className={classes.signatureDrinksItem__container}>
-      <h1 className={classes.signatureDrinksItem__container}></h1>
       <div className={classes.signatureDrinksItem__container}>
         <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/margarita.png`} alt="" />
         <img className={classes.signatureDrinksItem__Img} src={`${process.env.PUBLIC_URL}/images/drinksImg/paloma.png`} alt="" />
