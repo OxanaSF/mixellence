8551af95aa4628e75f5352bf715778f52741888e
traceWater <dalecottrell62@gmail.com>
2022-09-15 08:57:57 -0700

    changed css in DrinkCard

---
+++ src/data2/drinks.js
@@ -0,0 +1,24 @@
+export const DRINKS= [
+    {
+      id: 1,
+      name: 'Margarita',
+      quote: 'Made with the finest ingridents & our secret sauce',
+      linkImg:
+      `${process.env.PUBLIC_URL}/images/drinksImg/drink1.png`,
+    },
+    {
+      id: 2,
+      name: 'Paloma',
+      quote: '"Made with the finest ingridents & our secret sauce',
+      linkImg:
+      `${process.env.PUBLIC_URL}/images/drinksImg/drink2.png`,
+    },
+    {
+      id: 3,
+      name: 'Mojito',
+      quote: '"Made with the finest ingridents & our secret sauce',
+      linkImg:
+      `${process.env.PUBLIC_URL}/images/drinksImg/drink3.png`,
+    },
+  
+  ];
