c1582b9fbc51789979fe852ad236c82be2abc338
OxanaSF <oxanatango@gmail.com>
2022-09-15 18:57:00 -0700

    persist auth with local storage functionality

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AdminDashboard = () => {
   return (
     <main>
       <h1>Admin Dashboard</h1>
+      <button>
+        <Link to="/">Client Website</Link>
+      </button>
     </main>
   );
 };
