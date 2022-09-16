f230c9a667828d754be66b8640bab35ad97e9368
traceWater <dalecottrell62@gmail.com>
2022-09-16 13:58:59 -0700

    fixed error ln6 at SignatureDrink

--- src/components/SignatureDrink/DrinkCard.js
+++ src/components/SignatureDrink/DrinkCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import './SignatureDrink.css';
-import './SignatureDrink777.css';
-const DrinkCard = ({ id, linkImg, name, quote }) => {
+import './SignatureDrink.module.css';
 
+const DrinkCard = ({ id, linkImg, name, quote }) => {
+  
   return (
     <div className="drink-card-container" onInvalid={id}>
-      <h3 className="drink-card-top-name">{name}</h3>
       <div className="drink-card-top">
+        <h3 className="drink-card-top-name">{name}</h3>
         <img src={linkImg} alt={name} />
       </div>
 
--- src/components/SignatureDrink/SignatureDrinks.js
+++ src/components/SignatureDrink/SignatureDrinks.js
@@ -1,86 +1,21 @@
 import React from 'react';
-import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
 
-import DrinkCard from './DrinkCard';
 import { DRINKS } from '../../data/drinks';
 
-function SignatureDrinks() {
-  function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: 'none', background: 'red' }}
-        onClick={onClick}
-      />
-    );
-  }
+// import classes from './SignatureDrinks.module.css';
 
-  function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{ ...style, display: 'none', background: 'green' }}
-        onClick={onClick}
-      />
-    );
-  }
+import DrinkCard from './DrinkCard';
+
+function SignatureDrinks() {
+
 
   const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    initialSlide: 0,
-
-    // appendDots: (dots) => (
-    //   <div
-    //     style={{
-    //       backgroundColor: 'white',
-    //       borderRadius: '10px',
-    //       padding: '20px',
-    //     }}
-    //   >
-    //     <ul style={{ margin: '0px' }}> {dots} </ul>
-    //   </div>
-    // ),
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          nextArrow: <SampleNextArrow />,
-          prevArrow: <SamplePrevArrow />,
-        },
-      },
-    ],
+    
   };
 
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
+    <div className="container">
+      <div {...settings}>
         {DRINKS.map((item) => (
           <DrinkCard
             key={item.id}
@@ -90,7 +25,7 @@ function SignatureDrinks() {
             quote={item.quote}
           />
         ))}
-      </Slider>
+      </div>
     </div>
   );
 }
