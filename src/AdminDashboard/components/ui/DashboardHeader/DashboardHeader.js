d3c87f059c43f52fc250cafc26175b71e2ef4eb2
exionite <exionite@gmail.com>
2022-09-28 16:12:40 -0700

    drop nav styling

--- src/AdminDashboard/components/ui/DashboardHeader/DashboardHeader.js
+++ src/AdminDashboard/components/ui/DashboardHeader/DashboardHeader.js
@@ -1,12 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import classes from './DashboardHeader.module.css'
 
 const DashboardHeader = (props) => {
+  const [dropNav, setDropNav] = useState(false);
+
+  const dropNavHandler = () => {
+    setDropNav(!dropNav);
+  }
+
   return (
-    <header className={classes.dashboard_header}>
-        {props.children}
+    <header onClick={dropNavHandler} className={classes.dashboard_header}>
+      {props.children}
+      {dropNav && <div className={classes.dropNav}>
+        <NavLink
+          to="/about-dashboard"
+          className={({ isActive }) =>
+            isActive ? classes.nav_link_active : classes.nav_link
+          }
+        >
+          About
+        </NavLink>
+
+        <NavLink
+          to="/services-dashboard"
+          className={({ isActive }) =>
+            isActive ? classes.nav_link_active : classes.nav_link
+          }
+        >
+          Services
+        </NavLink>
+
+      </div>}
     </header>
   )
 }
