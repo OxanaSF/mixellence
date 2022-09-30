29115c2df2b5b390c6ff6752606812c7530658db
OxanaSF <oxanatango@gmail.com>
2022-09-29 22:33:17 -0700

    create Landing page on Dashboard, create asymetric image gallery using grid

--- src/AdminDashboard/components/HeroDashboard/HeroDisplayDashboard.js
+++ src/AdminDashboard/components/HeroDashboard/HeroDisplayDashboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import classes from './HeroDisplayDashboard.module.css';
-// import HeroBtn from '../../../ClientDisplay/components/Hero/Hero';
 
 const Hero = () => {
   return (
@@ -11,9 +10,11 @@ const Hero = () => {
         <h2>Modern, Fun, Professional, {'&'} Convenient</h2>
 
         <div className={classes.hero_business_info_wrapper}>
-          <img 
-          className={classes.montereyPin}
-          src={`${process.env.PUBLIC_URL}/images/pin.png`} alt='pin'/>
+          <img
+            className={classes.montereyPin}
+            src={`${process.env.PUBLIC_URL}/images/pin_white.png`}
+            alt="pin"
+          />
 
           <div className={classes.hero_business_info}>
             <p>Monterey Bay, California</p>
@@ -24,53 +25,48 @@ const Hero = () => {
         <button className={classes.heroButton}>Book a Consultation</button>
       </div>
 
-      <div className={classes.heroGalleryWrapper}>
-        GALLERY
-        {/* <div
-          className={`${classes.image1} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg1.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image2} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg2.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image3} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg3.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image4} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg4.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image5} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg5.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image6} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg6.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image7} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg7.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image8} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg8.png'}
-          alt=""
-        ></div>
-        <div
-          className={`${classes.image9} ${classes.bgImg}`}
-          src={process.env.PUBLIC_URL + '/images/heroImg/bg9.png'}
-          alt=""
-        ></div> */}
+      <div className={classes.image_gallery_wrapper}>
+        <div className={classes.image_gallery}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg1.png'}
+            alt=""
+          />
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg2.png'}
+            alt=""
+          />
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg3.png'}
+            alt=""
+          />
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg4.png'}
+            alt=""
+          />
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg5.png'}
+            alt=""
+          />
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg6.png'}
+            alt=""
+          />
+
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg7.png'}
+            alt=""
+          />
+
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg8.png'}
+            alt=""
+          />
+
+          <img
+            src={process.env.PUBLIC_URL + '/images/heroImg/bg9.png'}
+            alt=""
+          />
+        </div>
       </div>
     </section>
   );
