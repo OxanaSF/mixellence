e3df40fdb0cb6f633cee749f6ee0e382671a3e0e
exionite <exionite@gmail.com>
2022-10-04 14:34:18 -0700

    carousel working on admin testimonials

--- src/AdminDashboard/components/TestimonialsDashboard/ReactResponsiveCarousel.js
+++ src/AdminDashboard/components/TestimonialsDashboard/ReactResponsiveCarousel.js
@@ -6,12 +6,13 @@ import classes from './ReactCarousel.module.css'
 
 function ReactResponsiveCarousel(props) {
   return (
-    <Carousel 
-    // width={'100%'}
-    // infiniteLoop={true}
-    emulateTouch={true}
-    showStatus={false} 
-    className={classes.carousel}>
+    <Carousel
+      infiniteLoop={true}
+      showArrows={false}
+      emulateTouch={true}
+      showStatus={false}
+      className={classes.carousel}
+    >
       {props.children}
     </Carousel>
   )
