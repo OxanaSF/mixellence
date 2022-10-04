ba9a15634003762b464d929f08c0cf492c6083b6
exionite <exionite@gmail.com>
2022-10-04 15:06:45 -0700

    begin testimonial card carousel css

--- src/AdminDashboard/components/TestimonialsDashboard/TestimonialsDisplayDashboard.js
+++ src/AdminDashboard/components/TestimonialsDashboard/TestimonialsDisplayDashboard.js
@@ -53,7 +53,10 @@ function TestimonialsDisplay() {
       <Outlet />
 
       <div className={classes.card_display_container}>
+        
         {/* TODO  Can we change the amount of cards displayed in the carousel?? */}
+        {/* ADD some margin between dots and carousel card  */}
+
         <ReactResponsiveCarousel className={classes.reactCarousel}>
           {testimonials &&
             testimonials.map((item) => (
