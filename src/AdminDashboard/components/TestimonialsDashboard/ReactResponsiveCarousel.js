4aa17745e8a8b9340edbe2708f1ffc1e325beeeb
exionite <exionite@gmail.com>
2022-10-05 11:22:33 -0700

    initial state for windowWidth

--- src/AdminDashboard/components/TestimonialsDashboard/ReactResponsiveCarousel.js
+++ src/AdminDashboard/components/TestimonialsDashboard/ReactResponsiveCarousel.js
@@ -6,7 +6,7 @@ import classes from './ReactCarousel.module.css'
 
 function ReactResponsiveCarousel(props) {
   // const [widthJS, setWidthJS] = useState(0);
-  const [windowWidth, setWindowWidth] = useState(100);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth > 1300 && 33.3);
 
   window.addEventListener("resize", function () {
     if(this.window.innerWidth > 1300) {
@@ -15,15 +15,6 @@ function ReactResponsiveCarousel(props) {
       setWindowWidth(100)
     }
   })
-
-  // useEffect(() => {
-    // if (widthJS > 1100) {
-    //   setWindowWidth(33.3);
-    // } else {
-    //   setWindowWidth(100);
-    // }
-    // console.log(widthJS)
-  // }, [widthJS])
 
   return (
     <Carousel
