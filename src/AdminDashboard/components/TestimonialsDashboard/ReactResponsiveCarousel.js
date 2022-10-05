b179d79e86bd73509e91f635a547a8bd52494933
exionite <exionite@gmail.com>
2022-10-05 11:04:50 -0700

    testimonials slider now displays 3 cards at desktop width and 1 card for mobile. Add button is moving off screen, I am trying to keep it on the screen

--- src/AdminDashboard/components/TestimonialsDashboard/ReactResponsiveCarousel.js
+++ src/AdminDashboard/components/TestimonialsDashboard/ReactResponsiveCarousel.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel'
 
 import classes from './ReactCarousel.module.css'
 
 function ReactResponsiveCarousel(props) {
+  // const [widthJS, setWidthJS] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(100);
+
+  window.addEventListener("resize", function () {
+    if(this.window.innerWidth > 1300) {
+      setWindowWidth(33.3)
+    } else {
+      setWindowWidth(100)
+    }
+  })
+
+  // useEffect(() => {
+    // if (widthJS > 1100) {
+    //   setWindowWidth(33.3);
+    // } else {
+    //   setWindowWidth(100);
+    // }
+    // console.log(widthJS)
+  // }, [widthJS])
+
   return (
     <Carousel
-      infiniteLoop={true}
+      showThumbs={false}
+      // infiniteLoop={true}
       showArrows={false}
       emulateTouch={true}
+      centerSlidePercentage={windowWidth}
+      centerMode={true}
       showStatus={false}
       className={classes.carousel}
     >
